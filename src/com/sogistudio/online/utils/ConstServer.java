512d86247c6d010d81a6f2739adf1176bc77a19a
chaocraig <chao.craig@yahoo.com.tw>
2014-10-13 21:19:13 +0800

    Update ConstServer.java

--- src/com/sogistudio/online/utils/ConstServer.java
+++ src/com/sogistudio/online/utils/ConstServer.java
@@ -5,8 +5,8 @@ public class ConstServer {
 	/* Change to your server adress */
 	//public static final String BASE_URL                	= "http://officialapi.spikaapp.com";
 	//public static final String INFORMATION_URL			= "http://officialapi.spikaapp.com/page/information/";
-	public static final String BASE_URL                	= "http://ec2-54-68-13-103.us-west-2.compute.amazonaws.com/Sogi-Server/wwwroot";
-	public static final String INFORMATION_URL			= "http://ec2-54-68-13-103.us-west-2.compute.amazonaws.com/Sogi-Server/wwwroot/page/information/";
+	public static final String BASE_URL                	= "http://??.us-west-2.compute.amazonaws.com/Sogi-Server/wwwroot";
+	public static final String INFORMATION_URL			= "http://??.us-west-2.compute.amazonaws.com/Sogi-Server/wwwroot/page/information/";
 	public static final String LIST_SERVERS_URL         = BASE_URL + "/" + Const.API_FOLDER + "servers";
 	
 }
